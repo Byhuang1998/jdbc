dd1307ec38c8d517afab97dd5d9ae571257b81e4
byhuang1998 <byhuang1998@gmail.com>
2024-01-06 16:34:41 +0800

    init commit

---
+++ src/java/com.byhuang/JdbcApplication.java
@@ -0,0 +1,19 @@
+package com.byhuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * @author huangbingyi
+ * @version 1.0
+ * @date 2023/6/18 13:53
+ * @description TODO
+ */
+@SpringBootApplication
+public class JdbcApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext application = SpringApplication.run(JdbcApplication.class, args);
+        System.out.println();
+    }
+}
