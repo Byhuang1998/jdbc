7b89b5fb231e5b7760aeb3da6d7aee6d2c0b457c
byhuang1998 <byhuang1998@gmail.com>
2024-01-07 15:41:46 +0800

    JDBC connection transaction

---
+++ src/java/com.byhuang/api/ConnectionDemo.java
@@ -0,0 +1,46 @@
+package com.byhuang.api;
+
+import com.byhuang.util.JDBCUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * 连接数据库对象
+ */
+public class ConnectionDemo {
+
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = JDBCUtil.getConnection();
+        Statement statement = null;
+
+        // 关闭自动提交：true为自动提交事务；false为手动提交事务，即为开启事务
+        connection.setAutoCommit(false);
+        try {
+
+            // 定义sql
+            String sql1 = "update user set phone = '777' where id = 1";
+
+            String sql2 = "update user set phone = '777' where id = 2";
+
+            // 获取执行sql的对象
+            statement = connection.createStatement();
+            // 执行更新语句
+            statement.executeUpdate(sql1);
+            statement.executeUpdate(sql2);
+            // 提交事务
+            connection.commit();
+        } catch (Exception e) {
+            // 回滚
+            connection.rollback();
+            e.printStackTrace();
+        } finally {
+
+            // 释放资源
+            connection.close();
+            statement.close();
+        }
+    }
+}
